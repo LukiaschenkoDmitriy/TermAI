7aadf58fccfac0c6660086a42299b36244dc7543
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2025-05-08 19:10:37 +0200

    refactor: remove context management from parser

--- pkg/cmd/parser/parser.go
+++ pkg/cmd/parser/parser.go
@@ -1,8 +1,11 @@
 package parser
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os/exec"
+	"strings"
 
 	"github.com/LukiaschenkoDmitriy/TermAI/pkg/config"
 	"github.com/LukiaschenkoDmitriy/TermAI/pkg/http/client"
@@ -15,6 +18,7 @@ type CMDParser struct {
 	RootCMD *cobra.Command
 	ConfigCMD *cobra.Command
 	ExecuteCMD *cobra.Command
+	client *client.Client
 }
 
 func New() *CMDParser {
@@ -38,9 +42,11 @@ func New() *CMDParser {
 			Use:   "execute",
 			Short: "Execute TermAI",
 		},
-	};
+	}
 
-	return parser;
+	parser.client = client.New()
+
+	return parser
 }
 
 func (parser *CMDParser) Init() {
@@ -84,13 +90,58 @@ func (parser* CMDParser) ConfigRun(cmd *cobra.Command, args []string) {
 }
 
 func (parser* CMDParser) ExecuteRun(cmd *cobra.Command, args []string) {
-	client := client.New();
+	parser.AddRulesToMessage(parser.Config.Settings.Rules)
 
-	response, err := client.SendRequest([]string{parser.LastMessage})
+	response, err := parser.client.SendRequest([]string{parser.LastMessage})
 	if err != nil {
 		log.Fatalf("Failed to send request: %v", err)
 	}
-	fmt.Println(string(response))
+
+	// Debug: Print raw content with escaped characters
+	fmt.Printf("Raw content (with escapes): %q\n", response.Choices[0].Message.Content)
+
+	// Remove markdown code block markers
+	content := response.Choices[0].Message.Content
+	content = strings.TrimPrefix(content, "```json\n")
+	content = strings.TrimSuffix(content, "\n```")
+
+	var parsedContent struct {
+		Answer   string   `json:"answer"`
+		Commands []string `json:"commands"`
+		Error    string   `json:"error"`
+	}
+
+	if err := json.Unmarshal([]byte(content), &parsedContent); err != nil {
+		log.Printf("Failed to parse content. Error: %v\n", err)
+		log.Printf("Content that caused error: %s\n", content)
+		log.Fatal("Exiting due to parsing error")
+	}
+
+	fmt.Println(parsedContent.Answer)
+
+	if len(parsedContent.Commands) > 0 {
+		fmt.Println("\nExecuting commands:")
+		for _, cmd := range parsedContent.Commands {
+			cmd = strings.Replace(cmd, "echo '", "echo \"", -1)
+			cmd = strings.Replace(cmd, "' >", "\" >", -1)
+			cmd = strings.Replace(cmd, "' >>", "\" >>", -1)
+
+			fmt.Printf("\nExecuting: %s\n", cmd)
+			execCmd := exec.Command("bash", "-c", cmd)
+			output, err := execCmd.CombinedOutput()
+			if err != nil {
+				fmt.Printf("Error executing command: %v\n", err)
+				continue
+			}
+			fmt.Printf("Output:\n%s\n", output)
+		}
+	}
+}
+
+func (parser* CMDParser) AddRulesToMessage(rules []string) {
+	for _, rule := range rules {
+		parser.LastMessage += fmt.Sprintf("Rule: %s\n", rule)
+	}
 }
 
 func (parser* CMDParser) Execute() error {
