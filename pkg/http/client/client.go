87ee11d990fc1c58c5ee54b1d0af3fb92565a09a
Dmytrii Lukiashchenko <lukiaschenkoff@gmail.com>
2025-05-17 04:59:36 +0200

    refactor: improve message history conversion

--- pkg/http/client/client.go
+++ pkg/http/client/client.go
@@ -88,6 +88,46 @@ func (c *Client) AddMessages(role string,messages []string) {
 	}
 }
 
+func (c *Client) SendRequestWithoutContext(messages []string) (*response.Response, error) {
+	requestBody := make(map[string]any)
+	
+	requestBody["model"] = c.Model
+	requestBody["store"] = false
+	requestBody["messages"] = []Message{
+		{
+			Role: "system",
+			Content: strings.Join(messages, "\n"),
+		},
+	}
+
+	jsonBody, err := json.Marshal(requestBody)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", OPENAI_URL, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.APIKey))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.ParseResponse(body)
+}
+
 func (c *Client) SendRequest(messages []string) (*response.Response, error) {
 	requestBody := make(map[string]any)
 
